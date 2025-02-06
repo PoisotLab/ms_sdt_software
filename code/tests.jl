485812bef2c02c538fd1f8d276b3e306f04a59aa
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-05 22:00:42 -0500

    test

--- code/tests.jl
+++ code/tests.jl
@@ -2,18 +2,25 @@ using SpeciesDistributionToolkit
 using CairoMakie
 
 place = SpeciesDistributionToolkit.gadm("ARG")
+extent = SpeciesDistributionToolkit.boundingbox(place)
 
 tx = taxon("Hydrochoerus hydrochaeris")
 occ = occurrences(tx, "country" => "AR", "occurrenceStatus" => "PRESENT", "hasCoordinate" => true, "limit" => 300)
+occurrences!(occ)
+occurrences!(occ)
 
-provider = RasterData(CHELSA2, BioClim)
+provider = RasterData(WorldClim2, BioClim)
 
-temp = SDMLayer(provider; layer="BIO1", SpeciesDistributionToolkit.boundingbox(place)...)
+temp = SDMLayer(provider; layer="BIO1", extent...)
+mask!(temp, place)
 
 # Initial map
 f = Figure()
-ax = Axis(f[1, 1])
-heatmap!(ax, temp, color=:matter)
-scatter!(ax, occ, color=:white, strokecolor=:black, strokewidth=4)
+ax = Axis(f[1, 1], aspect=DataAspect())
+heatmap!(ax, temp, colormap=:thermal)
+lines!(ax, place, color=:black)
+scatter!(ax, mask(occ, place), color=:white, strokecolor=:black, strokewidth=2)
+hidedecorations!(ax)
+hidespines!(ax)
 f
 
